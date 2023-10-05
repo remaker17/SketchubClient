9033a7c75a60fbbdf7adcdfa8236f9818c9d6433
TheRemakerMan <farzasz7@gmail.com>
2023-10-05 08:50:22 +0700

    feat: Added new theme colors

---
+++ app/src/main/java/dev/remaker/sketchubx/ui/fragments/compose/DetailFragment.kt
@@ -0,0 +1,24 @@
+package dev.remaker.sketchubx.ui.fragments.compose
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+
+class DetailFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+        setContent {
+            MaterialTheme {
+                Text("Hello from Compose!")
+            }
+        }
+    }
+}
